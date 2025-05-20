1fbd1b1057ef585003b7d0d24f33b3c81610f5b0
mnmfullmetal <mnm.fullmetal@gmail.com>
2025-05-21 03:50:23 +1000

    - made the navbar animated and dynamic
    - played around with different color schemes
    
    - touched up some stylistic choices such as aligning text properly.

--- portfolio.js
+++ portfolio.js
@@ -6,3 +6,19 @@ function toggleResponsiveness() {
       x.className = "topnav";
     }
   }
+
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  var navbar = document.getElementById("myTopnav")
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    navbar.style.fontSize = "1rem";
+    navbar.style.backgroundColor = "#0a0216"
+    navbar.style.opacity = "95%"
+
+  } else {
+    navbar.style.fontSize = "2.5rem";
+    navbar.style.backgroundColor = "#0b001f";
+    navbar.style.opacity = "100%"
+  }
+}
